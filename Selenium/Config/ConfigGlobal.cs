aa60982f49de943bbd929ce6c9977f6535b83e40
dv.admin <dilipvaghela@ukflooringdirect.co.uk>
2022-09-08 14:12:10 +0100

    Selenium case study

--- Selenium/Config/ConfigGlobal.cs
+++ Selenium/Config/ConfigGlobal.cs
@@ -1,1 +1,13 @@
 ﻿global using NUnit.Framework;
+global using NUnit.Framework.Interfaces;
+global using OpenQA.Selenium;
+global using OpenQA.Selenium.Support.UI;
+global using System.Net.NetworkInformation;
+global using System.Reflection;
+global using System;
+global using OpenQA.Selenium.Chrome;
+global using OpenQA.Selenium.Firefox;
+global using OpenQA.Selenium.IE;
+global using OpenQA.Selenium.Remote;
+global using OpenQA.Selenium.Edge;
+global using OpenQA.Selenium.Safari;
--- Selenium/Tests/TestScripts/Desktop/TestBaseDesktop.cs
+++ Selenium/Tests/TestScripts/Desktop/TestBaseDesktop.cs
@@ -1,15 +1,53 @@
-﻿namespace Selenium;
+﻿
+using Selenium.Helper;
+[assembly: LevelOfParallelism(5)]
+namespace Selenium.Tests.TestScripts.Desktop;
 
-public class Tests
-{
+//Local drivers
+[TestFixture("Local", "Desktop - Chrome")]
+//[TestFixture("Local", "Desktop - Firefox")]
+//[TestFixture("Local", "Desktop - Edge")]
+//[TestFixture("Local", "Desktop - IE")]
+
+//Browserstack
+//[TestFixture("Browserstack", "Safari-Latest")]
+//[TestFixture("Browserstack", "Safari-13")]
+//[TestFixture("Browserstack", "Safari-12")]
+//[TestFixture("Browserstack", "Safari-11")]
+//[TestFixture("Browserstack", "Chrome-Latest")]
+//[TestFixture("Browserstack", "Chrome-Beta")]
+//[TestFixture("Browserstack", "Edge-Latest")]
+//[TestFixture("Browserstack", "Edge-Beta")]
+//[TestFixture("Browserstack", "FireFox-Latest")]
+//[TestFixture("Browserstack", "FireFox-Beta")]
+//[TestFixture("Browserstack", "IE11-Latest")]
+
+//Nunit attributes
+[Parallelizable(ParallelScope.All)]
+[FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+public class TestBaseDesktop
+{ 
+    private string profile = string.Empty;
+    private string browser = string.Empty;
+    public IWebDriver? webDriver;
+
+
+    public TestBaseDesktop(string profile, string browser)
+    {
+        this.profile = profile;
+        this.browser = browser;
+    }
+
     [SetUp]
     public void Setup()
-    {
+    { 
+            webDriver = new DriverHelpers().CreateDriver(profile, browser);
+            webDriver.Manage().Window.Maximize();
     }
 
-    [Test]
-    public void Test1()
+    [TearDown]
+    public void TearDown()
     {
-        Assert.Pass();
-    }
+            new SeleniumHelper(webDriver).Quit();   
+    } 
 }
